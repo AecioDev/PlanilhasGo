c6633b0d6e0a295fd67dbb21f4efcf949aa57d13
AecioDev <espiranda.ms@gmail.com>
2025-02-21 16:55:25 -0400

    fix: Removendo dados sensíveis

--- db/database.go
+++ db/database.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"log"
+	"planilhas/config"
 
 	"github.com/jackc/pgx/v4"
 )
@@ -14,13 +15,7 @@ type DBConnection struct {
 // Conecta ao banco de dados
 func ConnectToDB(amb string) (DBConnection, error) {
 
-	connStr := "postgres://postgres:12345@localhost:5432/portal_conhecimento"
-	switch amb {
-	case "hom":
-		connStr = "postgres://postgres:MastyJ8675@s0925.ms:5432/portal_conhecimento"
-	case "dev":
-		connStr = "postgres://postgres:MastyJ8675@s0925.ms:5432/portal_conhecimento_dev"
-	}
+	connStr := config.GetStrConnection(amb)
 	conn, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
 		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
